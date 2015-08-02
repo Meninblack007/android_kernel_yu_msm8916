a59f408b9ae2e415b1e9d176b6f517e3a4887805
Men_in_black007 <sanyam.53jain@gmail.com>
2015-08-02 10:07:14 -0400

    almighty: Change Toolchain
    Signed-off-by: Men_in_black007 <sanyam.53jain@gmail.com>

--- build.sh
+++ build.sh
@@ -25,13 +25,13 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 # Modify the following variable if you want to build
-export CROSS_COMPILE="/home/sanyam/sm_aarch64-linux-android-4.9/bin/aarch64-linux-android-"
-export LD_LIBRARY_PATH=/home/sanyam/sm_aarch64-linux-android-4.9/lib/
+export CROSS_COMPILE="/home/sanyam/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export LD_LIBRARY_PATH=/home/sanyam/aarch64-linux-android-4.9/lib/
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="Men_in_black007"
 export KBUILD_BUILD_HOST="AlmightyMachine"
-STRIP="/home/sanyam/sm_aarch64-linux-android-4.9/bin/aarch64-linux-android-strip"
+STRIP="/home/sanyam/aarch64-linux-android-4.9/bin/aarch64-linux-android-strip"
 MODULES_DIR=/home/sanyam/almighty/common
 OUT_DIR=/home/sanyam/almighty/tomato
 
